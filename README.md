053f43d76704c2700837ff040cb443e4e7e6526d
Water Dispenser <waterdispenser8@gmail.com>
2023-12-20 00:34:47 +0800

    changed title, added license & readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# telegram-channel-cleaner
+
+`telegram-channel-cleaner` is a Python program that can list and remove Telegram channel subscribers.
+
+## Commands
+
+```bash
+list | generates an excel file containing all subscribers in the specified Telegram channel
+kick | kicks selected users from the specified Telegram channel
+change | switches Telegram channel
+```
+
+## Download
+
+You can [download]("https://github.com/jakekeech/telegram-channel-cleaner/releases") the latest installable version of `telegram-channel-cleaner` for Windows.
--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def main():
     print(
         Panel(
             "\n[bold]Available Commands:[/bold]\n\n[bold #007C24]list[/bold #007C24] - Generate an excel file with a list of current subscribers in a Telegram channel\n\n[bold #007C24]kick[/bold #007C24] - Input a list of subscribers by ID to remove them in bulk\n\n[bold #007C24]change[/bold #007C24] - Select a different Telegram channel\n\n[bold #007C24]exit[/bold #007C24] - Exit the program :wave:\n",
-            title="[bold green]Telegram Channel Cleaner[/bold green]",
+            title="[bold green]telegram-channel-cleaner[/bold green]",
             subtitle="[italic]v0.1",
         )
     )
